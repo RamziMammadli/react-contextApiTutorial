05e99952615e878722cd3a934bf85d0a6829ffc0
John Doe <sdfinkod@code.edu.az>
2024-11-22 11:56:18 +0400

    context api

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React, { useContext } from 'react'
+import { MainContext } from '../context/context'
+
+const Header = () => {
+    
+    
+
+  return (
+   <div>asdasda</div>
+  )
+}
+
+export default Header
---
+++ src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Header from '../components/Header'
+import { useTheme } from '../context/context'
+
+
+const Home = () => {
+
+    const {theme, toggleTheme} = useTheme()
+    
+
+
+  return (
+    <div style={{backgroundColor:theme === 'light' ? 'white' : 'black', color:theme === 'light' ? 'black' : 'white'}}>
+        <button onClick={toggleTheme}>Change Mode</button>
+        <p>Alo</p>
+        <p>Alo</p>
+        <p>Alo</p>
+        <p>Alo</p>
+        <p>Alo</p>
+        <p>Alo</p>
+        <p>Alo</p>
+        <p>Alo</p>
+        <p>Alo</p>
+        <p>Alo</p>
+        <p>Alo</p>
+        <p>Alo</p>
+        <Header />
+    </div>
+  )
+}
+
+export default Home
